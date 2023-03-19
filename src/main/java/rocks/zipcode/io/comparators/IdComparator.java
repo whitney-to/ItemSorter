052ecf79ef2eb7406efb0c5e2fcd977f6ec0af80
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-19 17:28:34 -0400

    finished all with tests passing

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,16 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return (int) (o1.getId()-o2.getId());
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        if(o1.getPrice() > o2.getPrice()){
+            return 1;
+        } else if(o1.getPrice() < o2.getPrice()){
+            return -1;
+        }
+        return 0;
+    }
 }
